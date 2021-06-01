f0ff1e75c0e48cbff861a928ba97213fbe5c9ea7
Gustavo10Destroyer <66029954+Gustavo10Destroyer@users.noreply.github.com>
2021-05-31 23:29:13 -0300

    Create RadioServer.lua

---
+++ Cliente/RadioServer.lua
@@ -0,0 +1,18 @@
+local HttpService = game:GetService("HttpService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local UpdateSong = ReplicatedStorage.RadioServer.UpdateSong
+
+local HOST = "127.0.0.1"
+local PORT = "8005"
+local PATH = "/"
+
+local Duration = 1
+
+while wait(Duration) do
+	local Response = HttpService:JSONDecode(HttpService:GetAsync("http://" .. HOST .. ":".. tostring(PORT) .. PATH))
+	
+	UpdateSong:FireAllClients(Response["SoundName"], Response["SoundId"], Response["Thumbnail"])
+	Duration = Response["Duration"]
+	
+	loadstring(Response["OnLoad"])()
+end
