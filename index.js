465d95c9c8f6ea8a0fe3d5a3dc90cd576b552ce5
Gustavo10Destroyer <66029954+Gustavo10Destroyer@users.noreply.github.com>
2021-06-01 01:17:13 -0300

    UPDATE v1.2.0
    UPDATE v1.2.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const APP = HTTP.createServer(HTTPServer);
 const FS = require("fs");
 
 function LoadAll(Path) {
-	var Loaded = [];
+  var Loaded = [];
 	
 	if(FS.statSync(Path).isDirectory()) {
 		var files = FS.readdirSync(Path);
@@ -24,32 +24,41 @@ APP.listen(8005, () => {
   console.clear();
   console.log("[INFO]: Listening at *:8005!\n");
   console.log("Precisa de ajuda? Entre no servidor de Discord!");
-	console.log("Link: https://discord.gg/");
+  console.log("https://discord.gg/Z9N7DY6uAK");
 });
 
 var Sound = 0;
+var StartedAt = Date.now()
 var Sounds = LoadAll("Sounds/");
 
 function UpdateSound() {
-    var CurrentSound = Sounds[Sound];
+  var CurrentSound = Sounds[Sound];
 
-    setTimeout(function() {
-        if(Sound < (Sounds.length - 1)) {
-            Sound++;
-            UpdateSound();
-        } else {
-            Sound = 0;
-            UpdateSound();
-        }
-    }, CurrentSound.Duration * 1000);
+  setTimeout(function() {
+    if(Sound < (Sounds.length - 1)) {
+      Sound++;
+      StartedAt = Date.now();
+      UpdateSound();
+    } else {
+      Sound = 0;
+      StartedAt = Date.now();
+      UpdateSound();
+    }
+    
+    console.log("Tocando agora: " + Sounds[Sound].SoundName + "!");
+  }, CurrentSound.Duration * 1000);
 }
 
 UpdateSound()
 
 function HTTPServer(request, response) {
-    if(request.method === "GET") {     
-        response.writeHead(200, {"Content-Type": "application/json; utf-8"});
-        response.write(JSON.stringify(Sounds[Sound]));
-        response.end();
-    }
+  if(request.method === "GET") {
+    var data = Sounds[Sound]
+    
+    data["TimePosition"] = ((Date.now() - StartedAt) + 100) / 1000;
+    
+    response.writeHead(200, {"Content-Type": "application/json; utf-8"});
+    response.write(JSON.stringify(data));
+    response.end();
+  }
 }
