e45a62bd234aa3e7e83759742b64318169d20e49
Gustavo10Destroyer <66029954+Gustavo10Destroyer@users.noreply.github.com>
2021-06-01 02:12:26 -0300

    UPDATE v1.2.0!

--- index.js
+++ index.js
@@ -3,59 +3,59 @@ const APP = HTTP.createServer(HTTPServer);
 const FS = require("fs");
 
 function LoadAll(Path) {
-  var Loaded = [];
+    var Loaded = [];
 	
-	if(FS.statSync(Path).isDirectory()) {
-		var files = FS.readdirSync(Path);
-		
-		files.forEach((filename) => {
-			if(filename.endsWith(".json")) {
-				if(FS.statSync(Path+filename).isFile()) {
-					Loaded.push(JSON.parse(FS.readFileSync(Path+filename, "utf-8")))
-				}
-			}
-		});
-	}
-	
-	return Loaded;
+    if(FS.statSync(Path).isDirectory()) {
+        var files = FS.readdirSync(Path);
+        
+        files.forEach((filename) => {
+            if(filename.endsWith(".json")) {
+                if(FS.statSync(Path+filename).isFile()) {
+                    Loaded.push(JSON.parse(FS.readFileSync(Path+filename, "utf-8")))
+                }
+            }
+        });
+    }
+
+    return Loaded;
 }
 
 APP.listen(8005, () => {
-  console.clear();
-  console.log("[INFO]: Listening at *:8005!\n");
-  console.log("Precisa de ajuda? Entre no servidor de Discord!");
-  console.log("https://discord.gg/Z9N7DY6uAK");
+    console.clear();
+    console.log("[INFO]: Listening at *:8005!\n");
+    console.log("Precisa de ajuda? Entre no servidor de Discord!");
+    console.log("https://discord.gg/Z9N7DY6uAK");
 });
 
 var Sound = 0;
-var StartedAt = Date.now()
+var StartedAt = Date.now() - 100000;
 var Sounds = LoadAll("Sounds/");
 
 function UpdateSound() {
-  var CurrentSound = Sounds[Sound];
+    var CurrentSound = Sounds[Sound];
 
-  setTimeout(function() {
-    if(Sound < (Sounds.length - 1)) {
-      Sound++;
-      StartedAt = Date.now();
-      UpdateSound();
-    } else {
-      Sound = 0;
-      StartedAt = Date.now();
-      UpdateSound();
-    }
-    
-    console.log("Tocando agora: " + Sounds[Sound].SoundName + "!");
-  }, CurrentSound.Duration * 1000);
+    setTimeout(function() {
+        if(Sound < (Sounds.length - 1)) {
+            Sound++;
+            StartedAt = Date.now();
+            UpdateSound();
+        } else {
+            Sound = 0;
+            StartedAt = Date.now();
+            UpdateSound();
+        }
+        
+        console.log("Tocando agora: " + Sounds[Sound].SoundName + "!");
+    }, (CurrentSound.Duration - ((Date.now() - StartedAt) / 1000)) * 1000);
 }
 
 UpdateSound()
 
 function HTTPServer(request, response) {
   if(request.method === "GET") {
-    var data = Sounds[Sound]
+    var data = Sounds[Sound];
     
-    data["TimePosition"] = ((Date.now() - StartedAt) + 100) / 1000;
+    data["TimePosition"] = (Date.now() - StartedAt) / 1000;
     
     response.writeHead(200, {"Content-Type": "application/json; utf-8"});
     response.write(JSON.stringify(data));
