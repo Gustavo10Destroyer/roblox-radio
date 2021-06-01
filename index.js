f692e94c3f256f8dc4a5a0107b6cb8f61faf78de
Gustavo10Destroyer <66029954+Gustavo10Destroyer@users.noreply.github.com>
2021-06-01 00:57:14 -0300

    UPDATE v1.1.0

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const HTTP = require("http");
 const APP = HTTP.createServer(HTTPServer);
 const FS = require("fs");
 
+function LoadAll(Path) {
+	var Loaded = [];
+	
+	if(FS.statSync(Path).isDirectory()) {
+		var files = FS.readdirSync(Path);
+		
+		files.forEach((filename) => {
+			if(filename.endsWith(".json")) {
+				if(FS.statSync(Path+filename).isFile()) {
+					Loaded.push(JSON.parse(FS.readFileSync(Path+filename, "utf-8")))
+				}
+			}
+		});
+	}
+	
+	return Loaded;
+}
+
 APP.listen(8005, () => {
-    console.clear();
-    console.log("[INFO]: Listening at *:8005!");
+  console.clear();
+  console.log("[INFO]: Listening at *:8005!\n");
+  console.log("Precisa de ajuda? Entre no servidor de Discord!");
+	console.log("Link: https://discord.gg/");
 });
 
-var Sound = 0
-var Sounds = [
-    JSON.parse(FS.readFileSync("Sounds/Geoxor - Euphoria.json", "utf-8")),
-    JSON.parse(FS.readFileSync("Sounds/Geoxor - Moonlight.json", "utf-8")),
-    JSON.parse(FS.readFileSync("Sounds/Panda Eyes - Lonely Island.json", "utf-8"))
-];
+var Sound = 0;
+var Sounds = LoadAll("Sounds/");
 
 function UpdateSound() {
     var CurrentSound = Sounds[Sound];
