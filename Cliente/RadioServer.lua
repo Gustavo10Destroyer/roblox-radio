7a31d977568f61f625f2cb3be7f2e038ae454be5
Gustavo10Destroyer <66029954+Gustavo10Destroyer@users.noreply.github.com>
2021-06-01 02:11:58 -0300

    UPDATE v1.2.0!

--- Cliente/RadioServer.lua
+++ Cliente/RadioServer.lua
@@ -4,17 +4,17 @@ local HttpService = game:GetService("HttpService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local UpdateSong = ReplicatedStorage.RadioServer.UpdateSong
 
-local HOST = "127.0.0.1"
-local PORT = "8005"
+local HOST = "953da5ee9340.ngrok.io"
+local PORT = "80"
 local PATH = "/"
 
 local Duration = 1
 
 while wait(Duration) do
 	local Response = HttpService:JSONDecode(HttpService:GetAsync("http://" .. HOST .. ":".. tostring(PORT) .. PATH))
-	
+
 	UpdateSong:FireAllClients(Response)
-	Duration = Response["Duration"]
-	
+	Duration = Response["Duration"] - Response["TimePosition"]
+
 	loadstring(Response["OnLoad"])()
 end
