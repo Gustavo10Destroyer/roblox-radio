8ac9e8f0e30ca7e753c38b4e89f58f83c1bdffa9
Gustavo10Destroyer <66029954+Gustavo10Destroyer@users.noreply.github.com>
2021-06-01 01:21:04 -0300

    UPDATE v1.2.0!

--- Cliente/RadioClient.lua
+++ Cliente/RadioClient.lua
@@ -7,12 +7,13 @@ local Sound = Instance.new("Sound", game.Workspace)
 --local Thumbnail = game.Workspace.Part.RadioUI.Thumbnail
 --local SoundName = game.Workspace.Part.RadioUI.SoundName
 
-UpdateSong.OnClientEvent:Connect(function(Name, ID, Thumb)
+UpdateSong.OnClientEvent:Connect(function(Data)
 	Sound:Stop()
 	
-	Sound.SoundId = ID
-	--SoundName.Text = Name
-	--Thumbnail.Image = Thumb
+        Sound.SoundId = Data.SoundId
+        Sound.TimePosition = Data.TimePosition
+	--SoundName.Text = Data.SoundName
+	--Thumbnail.Image = Data.Thumbnail
 	
 	Sound:Play()
 end)
